cc3792dc7025049327214aca04e593dedad51014
trezorot <95047869+trezorot@users.noreply.github.com>
2021-12-18 20:34:33 +0100

    Add files via upload

---
+++ Homework_01/JS/script.js
@@ -0,0 +1,8 @@
+// Homework
+
+console.log("--------Homework--------");
+
+var phone = 30 * 119.95;
+var tax = (5 / 100) * phone;
+var total = phone + tax;
+console.log("The price of 30 phones including the 5% tax rate is " + total + "$.");
